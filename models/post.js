d0193123e631c67676a2a5b317a30eac1635a8c2
Shahmeer Navid <shahmeernavid@gmail.com>
2014-09-17 08:27:28 -0400

    remove old models

--- models/post.js
+++ models/post.js
@@ -1,11 +1,11 @@
-var Sequelize = require('Sequelize');
+// var Sequelize = require('Sequelize');
 
-module.exports = function (db){
+// module.exports = function (db){
 
-	return db.define('Post', {
-		title: {type: Sequelize.STRING, validate: {notNull: true}},
-		author: {type: Sequelize.STRING, validate: {notNull: true}},
-		content: {type: Sequelize.TEXT, validate: {notNull: true, notEmpty: true}}
-	});
+// 	return db.define('Post', {
+// 		title: {type: Sequelize.STRING, validate: {notNull: true}},
+// 		author: {type: Sequelize.STRING, validate: {notNull: true}},
+// 		content: {type: Sequelize.TEXT, validate: {notNull: true, notEmpty: true}}
+// 	});
 
-};
+// };
